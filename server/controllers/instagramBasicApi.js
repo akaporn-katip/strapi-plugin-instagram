03ee1b4f8235e8453b9d3cbd0fe65a1bcebefb00
akaporn-katip <akaporn.katip@gmail.com>
2025-10-25 00:31:27 +0700

    chore: bump version to 1.3.3 in package.json and standardize string quotes in instagramBasicApi.js

--- server/controllers/instagramBasicApi.js
+++ server/controllers/instagramBasicApi.js
@@ -1,35 +1,36 @@
-'use strict';
+"use strict";
 
 module.exports = ({ strapi }) => ({
   async downloadImages(ctx) {
     const { body } = ctx.request;
     try {
       ctx.body = await strapi
-        .plugin('instagram')
-        .service('instagramBasicApi')
+        .plugin("instagram")
+        .service("instagramBasicApi")
         .downloadImages(body.force);
     } catch (err) {
       ctx.throw(500, err);
     }
   },
   async getImages(ctx) {
-    const { body } = ctx.request;
+    // const { body } = ctx.request;
     try {
       // Token refresh if necessary
-      ctx.body = await strapi
-        .plugin('instagram')
-        .service('instagramToken')
+      await strapi
+        .plugin("instagram")
+        .service("instagramToken")
         .checkTokenExpiration();
       // Download new images if necessary
-      ctx.body = await strapi
-        .plugin('instagram')
-        .service('instagramBasicApi')
+      await strapi
+        .plugin("instagram")
+        .service("instagramBasicApi")
         .downloadImages(true);
       // Return images from database
+      const params = ctx.request.body || ctx.query;
       ctx.body = await strapi
-        .plugin('instagram')
-        .service('instaimage')
-        .find(body);
+        .plugin("instagram")
+        .service("instaimage")
+        .find(params);
     } catch (err) {
       ctx.throw(500, err);
     }
