7d430bb3d4fa73b52b2aaccf5a28cac67cbbdd48
akaporn-katip <akaporn.katip@gmail.com>
2025-11-20 22:48:12 +0700

    chore: bump version to 1.3.8 in package.json and comment out cron job in bootstrap.js

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -2,19 +2,19 @@
 
 module.exports = ({ strapi }) => {
   // bootstrap phase
-  strapi.cron.add({
-    // runs every second
-    myJob: {
-      task: async ({ strapi }) => {
-        console.log("start refresh token");
-        await strapi
-          .plugin("instagram")
-          .service("instagramToken")
-          .checkTokenExpiration();
-      },
-      options: {
-        rule: "0 0 */5 * * *",
-      },
-    },
-  });
+  // strapi.cron.add({
+  //   // runs every second
+  //   myJob: {
+  //     task: async ({ strapi }) => {
+  //       console.log("start refresh token");
+  //       await strapi
+  //         .plugin("instagram")
+  //         .service("instagramToken")
+  //         .checkTokenExpiration();
+  //     },
+  //     options: {
+  //       rule: "0 0 */5 * * *",
+  //     },
+  //   },
+  // });
 };
